cfbd0644c798e71dba09fc5eb12429534d134271
michalovsk <92398112+michalovsk@users.noreply.github.com>
2024-01-21 13:09:53 +0100

    saving index before modal changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,14 +33,36 @@
             </div>
             <div class="row">
                 <div class="col-12 col-md-9 col-xl-5 mainPageText">
-                    <p id="textMainPage" >Lets play a game of typeracer! Press play to get going.</p>
+                    <p id="textMainPage" >Unleash the Speed of Your Fingertips: Type Faster, Race Harder, and Conquer the Keyboard! Welcome to TypeRacer, where precision meets velocity. Ready, Set, Type!</p>
                 </div>
             </div>
             <div class="col-6 col-md-3 text-center">
-                <form method="post" action="{{ route('viewGame') }}" accept-charset="UTF-8">
-                    {{ csrf_field() }}
-                    <button class="btn btn-primary w-100 playGameButton">Play</button>
-                </form>
+                <button class="btn btn-primary w-100 playGameButton" data-bs-toggle="modal" data-bs-target="#choose_text">Play</button>
+                <div class="modal fade" id="choose_text" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-lg">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="staticBackdropLabel">Choose your text</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form id="gameForm" method="post" action="{{ route('viewGame') }}" accept-charset="UTF-8">
+                                    {{ csrf_field() }}
+                                    <!-- Make sure you have an input field with the name "gameTextID" -->
+                                    <input type="hidden" name="gameTextID" id="gameTextIDInput" value="">
+
+                                    <div class="btn-group row g-2" role="group" id="gameTextID">
+                                        @foreach($gameTexts as $gameText)
+                                            <button type="button" class="btn btn-primary game-button" data-value="{{ $gameText->textName }}" data-id="{{$gameText->id}}" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; width: 20%;">
+                                                {{ Str::limit($gameText->textName, 40) }}
+                                            </button>
+                                        @endforeach
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         @else
             <div class="row g-2">
@@ -59,7 +81,7 @@
             </div>
             <div class="row">
                 <div class="col-12 col-md-9 col-xl-5 mainPageText">
-                    <p id="textMainPage" >Lets play a game of typeracer! Login to continue.</p>
+                    <p id="textMainPage" >Unleash the Speed of Your Fingertips: Type Faster, Race Harder, and Conquer the Keyboard! Welcome to TypeRacer, where precision meets velocity. Ready, Set, Type!</p>
                 </div>
             </div>
 
@@ -80,8 +102,14 @@
                     <button class="btn btn-primary w-100 leaderboardMenuButton">Leaderboard</button>
                 </form>
             </div>
-
     </div>
+    @if(session('message'))
+        <div class="alert alert-success">
+            {{ session('message') }}
+        </div>
+    @endif
+
+
     <footer class="row fixed-bottom" >
         <p>Author: Tibor Michalov <a href="mailto:michalov1@stud.uniza.sk">michalov1@stud.uniza.sk</a></p>
         <!--<p><a href="mailto:michalov1@stud.uniza.sk">michalov1@stud.uniza.sk</a></p>-->
